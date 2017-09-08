e67a0d87743cd48b1079d40e32b070dd31d23665
Binh Van Nguyen <binhnv80@gmail.com>
2017-09-08 10:08:16 -0700

    Update Kylo scripts

--- scripts/startup/002_kylo.sh
+++ scripts/startup/002_kylo.sh
@@ -6,6 +6,6 @@ g_host=localhost
 my_tcp_wait ${g_host} ${g_port}
 my_tcp_wait ${KYLO_NIFI_REST_HOST} ${KYLO_NIFI_REST_PORT}
 
-if [[ ! -z "${KYLO_INIT_SCRIPT}" ]]; then
-    bash ${KYLO_INIT_SCRIPT}
+if [[ ! -z "${KYLO_POSTSTART_SCRIPT}" ]]; then
+    bash ${KYLO_POSTSTART_SCRIPT}
 fi
--- templates/usr/local/kylo/kylo-services/bin/run-kylo-services.sh
+++ templates/usr/local/kylo/kylo-services/bin/run-kylo-services.sh
@@ -5,13 +5,14 @@ if [[ "${KYLO_SERVICES_OPTS}" == "" ]]; then
     export KYLO_SERVICES_OPTS=-Xmx768m
 fi
 
-[ -f /usr/local/kylo/encrypt.key ] && export ENCRYPT_KEY="$(cat /usr/local/kylo/encrypt.key)"
-PROFILES=$(grep ^spring.profiles. /usr/local/kylo/kylo-services/conf/application.properties)
-KYLO_NIFI_PROFILE="nifi-v1"
-if [[ ${PROFILES} == *"nifi-v1.2"* ]] || [[ ${PROFILES} == *"nifi-v1.3"* ]];
-then
- KYLO_NIFI_PROFILE="nifi-v1.2"
+[ -f ${KYLO_HOME}/encrypt.key ] && export ENCRYPT_KEY="$(cat ${KYLO_HOME}/encrypt.key)"
+PROFILES=$(grep ^spring.profiles. ${KYLO_HOME}/kylo-services/conf/application.properties)
+# find user provided nifi profile
+KYLO_NIFI_PROFILE=$(echo ${PROFILES} | sed -e 's/\(.*\),\?nifi-v\([^,]*\).*/nifi-v\2/g')
+# use nifi-v1 as default profile when profile is not provided or it is invalid
+if [[ "x${KYLO_NIFI_PROFILE}" == "x" ]] || [[ ! -d ${KYLO_HOME}/kylo-services/lib/${KYLO_NIFI_PROFILE} ]]; then
+    KYLO_NIFI_PROFILE="nifi-v1"
 fi
 echo "using NiFi profile: ${KYLO_NIFI_PROFILE}"
 
-java $KYLO_SERVICES_OPTS -cp /usr/local/kylo/kylo-services/conf:/usr/local/kylo/kylo-services/lib/*:/usr/local/kylo/kylo-services/lib/${KYLO_NIFI_PROFILE}/*:/usr/local/kylo/kylo-services/plugin/* com.thinkbiganalytics.server.KyloServerApplication --pgrep-marker=kylo-services-pgrep-marker > /var/log/kylo-services/kylo-services.log 2>&1 &
+java $KYLO_SERVICES_OPTS -cp ${KYLO_HOME}/kylo-services/conf:${KYLO_HOME}/kylo-services/lib/*:${KYLO_HOME}/kylo-services/lib/${KYLO_NIFI_PROFILE}/*:${KYLO_HOME}/kylo-services/plugin/* com.thinkbiganalytics.server.KyloServerApplication --pgrep-marker=kylo-services-pgrep-marker > /var/log/kylo-services/kylo-services.log 2>&1 &
